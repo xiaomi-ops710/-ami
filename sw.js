33545d99b7d3c8052e440ad12343e6c5340ff70b
xiaomi-ops710 <xiongdayiteng4@gmail.com>
2026-06-25 17:13:51 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+```javascript
+const CACHE_NAME = 'volcano-monitor-v1.4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// インストール時にファイルをキャッシュする
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// キャッシュからファイルを返す（オフライン対応）
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+```
